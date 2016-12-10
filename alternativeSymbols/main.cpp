1e3fec9463c1836633027fbc19c9d8293bdb2201
Yura Braiko <yurabraiko@gmail.com>
2016-12-10 14:30:07 +0200

    add alternative symbol simpel

---
+++ alternativeSymbols/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Yura Braiko on 10.12.16.
+//
+
+#include <iostream>
+
+using  namespace std;
+
+int main()<%
+    int a<:3:> = <%1,2,4%>;
+    cout << "hello word " << (a<:1:> bitor a<:2:>);
+    return  0;
+%>
